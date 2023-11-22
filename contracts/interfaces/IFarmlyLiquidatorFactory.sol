7e689a9a47d1e30544feeb2794eb61662d56dc7c
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2023-11-22 17:50:12 +0300

    describe the liquidator factory contract

---
+++ contracts/interfaces/IFarmlyLiquidatorFactory.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.5.0;
+
+/// @title The interface of the liquidator factory contract
+/// @notice FarmlyLiquidatorFactory is responsible for creating and managing FarmlyLiquidator contracts.
+/// It also holds the link tokens required for the contracts (for Chailink Automation).
+/// FarmlyLiquidator contracts request link tokens from here, factory transfers them.
+interface IFarmlyLiquidatorFactory {
+
+}
