cb3b624af59a3fb5bf6d1c55e21aa2de816b5601
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2023-11-22 18:07:08 +0300

    Create IFarmlyLiquidator.sol
    describe the liquidator contract

---
+++ contracts/interfaces/IFarmlyLiquidator.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.5.0;
+
+/// @title The interface of the liquidator contract
+/// @notice This contract constantly monitors positions on Farmly Finance.
+/// The positions are liquidated when the liquidation threshold is exceeded.
+/// Control operations and execution are provided by Chainlink (Chainlink Automation).
+interface IFarmlyLiquidator {
+
+}
