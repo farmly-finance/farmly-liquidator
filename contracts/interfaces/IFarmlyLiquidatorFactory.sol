28384d774e2ad5f76b9faa8ff77f205c6b37c4e1
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2023-11-22 18:53:32 +0300

    IFarmlyLiquidatorFactoryImmutables

--- contracts/interfaces/IFarmlyLiquidatorFactory.sol
+++ contracts/interfaces/IFarmlyLiquidatorFactory.sol
@@ -1,9 +1,10 @@
 pragma solidity >=0.5.0;
+import "./factory/IFarmlyLiquidatorFactoryImmutables.sol";
 
 /// @title The interface of the liquidator factory contract
 /// @notice FarmlyLiquidatorFactory is responsible for creating and managing FarmlyLiquidator contracts.
 /// It also holds the link tokens required for the contracts (for Chailink Automation).
 /// FarmlyLiquidator contracts request link tokens from here, factory transfers them.
-interface IFarmlyLiquidatorFactory {
+interface IFarmlyLiquidatorFactory is IFarmlyLiquidatorFactoryImmutables {
 
 }
