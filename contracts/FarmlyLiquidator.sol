65f8961e65c5eb7709976bae276cb76f392aac27
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2023-11-26 14:11:24 +0300

    FarmlyLiquidator implementation

---
+++ contracts/FarmlyLiquidator.sol
@@ -0,0 +1,102 @@
+pragma solidity >=0.5.0;
+
+import "./interfaces/IFarmlyLiquidator.sol";
+
+contract FarmlyLiquidator is IFarmlyLiquidator {
+    /// @inheritdoc IFarmlyLiquidatorImmutables
+    IFarmlyPositionManager public override farmlyPositionManager;
+    /// @inheritdoc IFarmlyLiquidatorImmutables
+    IFarmlyUniV3Reader public override farmlyUniV3Reader;
+    /// @inheritdoc IFarmlyLiquidatorImmutables
+    IFarmlyLiquidatorFactory public override farmlyLiquidatorFactory;
+    /// @inheritdoc IFarmlyLiquidator
+    uint256 public override liquidatorID;
+    /// @inheritdoc IFarmlyLiquidator
+    CheckRange public override checkRange;
+
+    constructor(uint256 _liquidatorID) {
+        farmlyLiquidatorFactory = IFarmlyLiquidatorFactory(msg.sender);
+        uint256 maxPositionPerLiquidator = farmlyLiquidatorFactory
+            .MAX_POSITION_PER_LIQUIDATOR();
+        liquidatorID = _liquidatorID;
+        checkRange = CheckRange(
+            (_liquidatorID - 1) * maxPositionPerLiquidator, // 0
+            _liquidatorID * maxPositionPerLiquidator // 5
+        );
+    }
+
+    /// @inheritdoc AutomationCompatibleInterface
+    function checkUpkeep(
+        bytes calldata checkData
+    )
+        external
+        view
+        override
+        returns (bool upkeepNeeded, bytes memory performData)
+    {
+        uint256 activePositionsLength = farmlyPositionManager
+            .getActivePositionsLength();
+        upkeepNeeded = false;
+        uint256 upkeepBalance = farmlyLiquidatorFactory.getUpkeepLinkBalance(
+            address(this)
+        );
+
+        if (
+            upkeepBalance <=
+            farmlyLiquidatorFactory.getUpkeepFundingThreshold(address(this))
+        ) {
+            upkeepNeeded = true;
+            performData = abi.encode(Operation.FundUpkeep, 0);
+        } else {
+            if (activePositionsLength > checkRange.min) {
+                uint256 from = checkRange.min;
+                uint256 to = checkRange.max > activePositionsLength
+                    ? activePositionsLength - 1
+                    : checkRange.max - 1;
+
+                for (uint i = from; i <= to; i++) {
+                    uint positionID = farmlyPositionManager.activePositions(i);
+                    uint flyScore = farmlyPositionManager.getFlyScore(
+                        positionID
+                    );
+                    if (flyScore >= 10000) {
+                        upkeepNeeded = true;
+                        performData = abi.encode(
+                            Operation.LiquidatePosition,
+                            positionID
+                        );
+                    }
+                }
+            }
+        }
+    }
+
+    /// @inheritdoc AutomationCompatibleInterface
+    function performUpkeep(bytes calldata performData) external override {
+        (Operation operation, uint positionID) = abi.decode(
+            performData,
+            (Operation, uint256)
+        );
+        if (operation == Operation.FundUpkeep) {
+            _fundUpkeep();
+        } else if (operation == Operation.LiquidatePosition) {
+            _liquidatePosition(positionID);
+        }
+    }
+
+    function _fundUpkeep() internal {
+        farmlyLiquidatorFactory.fundUpkeep();
+    }
+
+    function _liquidatePosition(uint256 positionID) internal {
+        if (farmlyPositionManager.getFlyScore(positionID) >= 10000)
+            farmlyPositionManager.liquidatePosition(
+                IFarmlyPositionManagerActions.LiquidatePositionParams(
+                    IFarmlyUniV3Executor(
+                        0x6C31c4AD95bbF35BfbE4F3DEcd9f60AF833F455c
+                    ),
+                    positionID
+                )
+            );
+    }
+}
