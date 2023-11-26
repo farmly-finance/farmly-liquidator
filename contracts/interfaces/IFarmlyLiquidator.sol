973acb597b0c4ffae7b5fad4387c8ea93bc0c9f9
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2023-11-26 13:33:16 +0300

    immutables

--- contracts/interfaces/IFarmlyLiquidator.sol
+++ contracts/interfaces/IFarmlyLiquidator.sol
@@ -1,9 +1,14 @@
 pragma solidity >=0.5.0;
+import "@chainlink/contracts/src/v0.8/automation/AutomationCompatible.sol";
+import "./IFarmlyLiquidatorImmutables.sol";
 
 /// @title The interface of the liquidator contract
 /// @notice This contract constantly monitors positions on Farmly Finance.
 /// The positions are liquidated when the liquidation threshold is exceeded.
 /// Control operations and execution are provided by Chainlink (Chainlink Automation).
-interface IFarmlyLiquidator {
+interface IFarmlyLiquidator is
+    IFarmlyLiquidatorImmutables,
+    AutomationCompatibleInterface
+{
 
 }
